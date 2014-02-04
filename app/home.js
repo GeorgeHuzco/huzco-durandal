7c53bb93e5cb308df714a5a2e9c93aa62af9227b
George Treacy <george@huzco.com>
2014-02-04 18:52:12 +1000

    figured out the ko binding. You have to use activate.

--- app/home.js
+++ app/home.js
@@ -1,4 +1,4 @@
-/*global define, console, $, document, window, videojs */
+/*global define, console, $, document, window, videojs, http */
 
 console.log(videojs);
 
@@ -7,11 +7,10 @@ define(function (require) {
 	
 	var app = require('durandal/app'),
 		ko = require('knockout'),
+		http = require('plugins/http'),
 		queryPrefix = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=',
 		apiKey = 'AIzaSyBRsops2-sGNOAR2KzHvZwYTgqjPWEbD9k',
 		Video,
-		vm,
-		search,
 		loadVideo,
 		player = null,
 		viewWidth = 1000, //document.getElementById("guts").clientWidth,
@@ -50,41 +49,40 @@ define(function (require) {
 		});
 	};
 	
-	search = function () {
-// 		if (!vm.query()) {
-// 			vm.query('arctic monkeys');
-//		}
-		var queryString = queryPrefix + (vm.query()).replace(" ", "+") + '&key=' + apiKey;
-		console.log('do the search for: ' + queryString);
-		
-		$.get(queryString, function (data, status) {
-			var i, l, vid, vidObj;
-			if (status === 'success') {
-				console.log(data);
-				vm.vids = []; // always reset the video array
-				for (i = 0, l = data.items.length; i < l; i += 1) {
-					vid = data.items[i];
+	return {
+		// vids: [new Video('9JCZT_51GA0'), new Video('OGPD0ZBiMs0')],
+		vids: ko.observableArray([]),
+		query: ko.observable('black sabbath'),
+		activate: function () {
+			var tempArray = [], queryString,
+				that = this;
+			
+			queryString = queryPrefix + (that.query()).replace(" ", "+") + '&key=' + apiKey;
+			console.log('do the search for: ' + queryString);
+			
+			return http.get(queryString).then(function (response) {
+				var i, l, vid, vidObj;
+				console.log('we got something in the promise.');
+				console.log(response);
+				that.vids([]); // always reset the video array
+				for (i = 0, l = response.items.length; i < l; i += 1) {
+					vid = response.items[i];
 					if (vid && vid.id && vid.id.videoId) {
 						console.log(vid.id.videoId);
 						vidObj = new Video(vid.id.videoId);
-						vm.vids.push(vidObj);
+						that.vids.push(vidObj);
 					}
 				}
-				if (vm.vids[0]) {
+				
+				if (that.vids[0]) {
 					vm.loadVideo(vm.vids[0].videoId);
 				}
-			} else {
-				console.log('GET Failed for video list. ' + status);
-			}
-		});
-	};
+				console.log('Number of vids: ' +  that.vids.length);
 	
-	vm = {
-		vids: [],
-		query: ko.observable('black sabbath'),
-		search: search,
+				
+				//that.images(response.items);
+			});
+		},
 		loadVideo: loadVideo
 	};
-	
-	return vm;
 });
