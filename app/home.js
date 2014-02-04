5a46be2ac294790aad3952604cdbdf0a4c31b62f
George Treacy <george@huzco.com>
2014-02-04 18:59:46 +1000

    refactor search function out of returned vm

--- app/home.js
+++ app/home.js
@@ -11,7 +11,8 @@ define(function (require) {
 		queryPrefix = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=',
 		apiKey = 'AIzaSyBRsops2-sGNOAR2KzHvZwYTgqjPWEbD9k',
 		Video,
-		loadVideo,
+		loadVideo, 
+		search,
 		player = null,
 		viewWidth = 1000, //document.getElementById("guts").clientWidth,
 		aspectRatio = 16 / 9,
@@ -49,44 +50,43 @@ define(function (require) {
 		});
 	};
 	
+	search = function () {
+		var tempArray = [], queryString,
+			that = this;
+			
+		if (that.query() === '') {
+			return;
+		}
+		
+		queryString = queryPrefix + (that.query()).replace(" ", "+") + '&key=' + apiKey;
+		console.log('do the search for: ' + queryString);
+		
+		return http.get(queryString).then(function (response) {
+			var i, l, vid, vidObj;
+			console.log('we got something in the promise.');
+			console.log(response);
+			that.vids([]); // always reset the video array
+			for (i = 0, l = response.items.length; i < l; i += 1) {
+				vid = response.items[i];
+				if (vid && vid.id && vid.id.videoId) {
+					console.log(vid.id.videoId);
+					vidObj = new Video(vid.id.videoId);
+					that.vids.push(vidObj);
+				}
+			}
+			
+			if (that.vids[0]) {
+				that.loadVideo(that.vids[0].videoId);
+			}
+			console.log('Number of vids: ' +  that.vids.length);
+		});
+	};
+	
 	return {
 		// vids: [new Video('9JCZT_51GA0'), new Video('OGPD0ZBiMs0')],
 		vids: ko.observableArray([]),
 		query: ko.observable(''),
-		activate: function () {
-			var tempArray = [], queryString,
-				that = this;
-				
-			if (that.query() === '') {
-				return;
-			}
-			
-			queryString = queryPrefix + (that.query()).replace(" ", "+") + '&key=' + apiKey;
-			console.log('do the search for: ' + queryString);
-			
-			return http.get(queryString).then(function (response) {
-				var i, l, vid, vidObj;
-				console.log('we got something in the promise.');
-				console.log(response);
-				that.vids([]); // always reset the video array
-				for (i = 0, l = response.items.length; i < l; i += 1) {
-					vid = response.items[i];
-					if (vid && vid.id && vid.id.videoId) {
-						console.log(vid.id.videoId);
-						vidObj = new Video(vid.id.videoId);
-						that.vids.push(vidObj);
-					}
-				}
-				
-				if (that.vids[0]) {
-					that.loadVideo(that.vids[0].videoId);
-				}
-				console.log('Number of vids: ' +  that.vids.length);
-	
-				
-				//that.images(response.items);
-			});
-		},
+		activate: search,
 		loadVideo: loadVideo
 	};
 });
